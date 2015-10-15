3109250966a65b3d346d9dbb9c79da99508d92a9
aparracorbacho <aparracorbacho@atanasoff18.danielcastelao.org>
2015-10-15 10:41:10 +0200

    Mas Pruebas

--- src/ejemplorectangulo/EjemploRectangulo.java
+++ src/ejemplorectangulo/EjemploRectangulo.java
@@ -1,15 +1,22 @@
 
 package ejemplorectangulo;
 
+import javax.swing.JOptionPane;
+
 public class EjemploRectangulo {
 
     
     public static void main(String[] args) {
-        Rectangulo rectangulo1 = new Rectangulo(3,5);
-        // System.out.println("Base " +rectangulo1.base); // Se e publico podese acceder así
+        float base, altura;
+        String baseTeclado = JOptionPane.showInputDialog("Introduce la base");
+        base = Float.parseFloat(baseTeclado);
+        String alturaTeclado = JOptionPane.showInputDialog("Introduce la altura");
+        altura = Float.parseFloat(alturaTeclado);
+        Rectangulo rectangulo1 = new Rectangulo(base,altura);
+        //System.out.println("Base " +rectangulo1.base); // Se e publico podese acceder así
         System.out.println("Altura: "+rectangulo1.getAltura());
         System.out.println("Base: "+rectangulo1.getBase());
-        System.out.println("Area de un rectangulo de 10 de base y 3 de altura "+rectangulo1.calcularArea(10,3));
+        System.out.println("Area de un rectangulo "+rectangulo1.calcularArea(base,altura));
         System.out.println("Perimetro del primer rectangulo "+rectangulo1.calcularPerimetro());
     }
     
